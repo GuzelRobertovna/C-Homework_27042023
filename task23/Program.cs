5228365b793c870542bf41fdf99a3486a59145a5
Guzel_Ayupova <guzel_robertovna@mail.ru>
2023-05-03 21:37:48 +0500

    Задача23. Нахождение куба чисел от 1 до N

---
+++ task23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number = Prompt("Введите число: ");
+int count = 1;
+while (count <= number)
+{
+    Console.Write(FindCubeNumbers(count) + " ");
+    count++;
+}
+Console.WriteLine();
+
+
+int FindCubeNumbers(int num)
+{
+    int cubeNum = num * num * num;
+    return cubeNum;
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine()!;
+    int result = Convert.ToInt32(value);
+    return result;
+}
